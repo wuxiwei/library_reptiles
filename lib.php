1079903061ebfe7521e6a6f22c0258b810cd4c9d
wxw <wxw_it@163.com>
2016-05-05 10:32:50 +0800

    实现图书爬虫

--- lib.php
+++ lib.php
@@ -1,6 +1,7 @@
 <?php
-$mysql=new PDO('mysql:host=localhost;dbname=cityuit','root','q123456');
-$res = $mysql->query('select * from library_books');
+error_reporting(E_ALL ^ E_NOTICE);
+/* $mysql=new PDO('mysql:host=localhost;dbname=cityuit','root','q123456'); */
+/* $res = $mysql->query('select * from library_books'); */
 /* foreach($res->fetchAll() as $row){ */
 /*     print "$row[title] is $row[auther] write"; */
 /*     print "\n"; */
@@ -25,12 +26,55 @@ $res = $mysql->query('select * from library_books');
 /*       } */
 /* } */
 
+main_action();
+
+function main_action(){
+    $no = "0000128831";
+    $bookmes = check_books($no);
+    if($bookarr['res'] == 201){
+        print "no=> $no ,mes=> no book for $bookmes[mes]";
+    }else if($bookmes['res'] == 501){
+        save_no($no);            
+        print "no=> $no ,mes=> no book for $bookmes[mes]";
+    }else{ 
+        $insert = save_book($bookmes);
+        if($insert['result'] === FALSE || $insert['result'] == 0){    //如果存储失败，和超时同样处理
+            save_no($no);            
+            print "no=> $no ,mes=> no book for mysql error";
+        }else{
+            print "no=> $no ,book for line $insert[lineId]";
+        }
+    }
+}
+
+function save_book($bookmes){
+    $pdo=new PDO('mysql:host=localhost;dbname=cityuit','root','q123456');
+    $pdo->query('set names utf8');//设置字符集
+    $strSql = "INSERT INTO `library_books`(`title`, `auther`, `press`, `time`, `search`, `place`, `state`) VALUES ('$bookmes[title]','$bookmes[auther]','$bookmes[press]','$bookmes[time]','$bookmes[search]','$bookmes[place]','$bookmes[state]')";
+    $result = $pdo->exec($strSql);//返回影响了多少行数据
+    $lineId = $pdo->lastInsertId();//返回刚插入的id(的自增id)
+    return array("result"=>$result, "lineId"=>$lineId);
+}
+
+function save_no($no){
+    $pdo=new PDO('mysql:host=localhost;dbname=cityuit','root','q123456');
+    $pdo->query('set names utf8');//设置字符集
+    $strSql = "INSERT INTO `timeout_no`(`no`) VALUES ('$no')";
+    $pdo->exec($strSql);//返回影响了多少行数据
+    /* $lineId = $pdo->lastInsertId();//返回刚插入的id(的自增id) */
+    /* return $lineId; */
+}
 
 function check_books($no){
     $content = http_get("http://210.30.108.79/opac/item.php?marc_no=$no");
 
+    if(!$content){
+        return array("res"=>501,"mes"=>"timeout");   //表示没有这本，并且是因为下面匹配为空导致，返回就直接终止
+        exit;
+    }
+
     $contents = preg_replace("/([\r\n|\n|\t| ]+)/",'',$content);  //为更好地避开换行符和空格等不定因素的阻碍，有必要先清除采集到的源码中的换行符、空格符和制表符
-    $contents = html_entity_decode($contents, ENT_QUOTES, 'GB2312');     //将&#x0020;字符转中文
+    $contents = html_entity_decode($contents);     //将&#x0020;字符转中文
 
     //先确定有没有这本书，然后去解析书的信息
     $bookarr = array();        
@@ -59,23 +103,23 @@ function check_books($no){
             $state = $out[1] == '可借' ? '可借' : '已借出';           //变量表示是否可借
         }
     }else{
-        return array("res"=>"201");   //表示没有这本，返回就直接终止
+        return array("res"=>201,"mes"=>"under");   //表示没有这本，并且是因为下面匹配为空导致，返回就直接终止
         exit;
     }
 
-    /* $preg = '/题名\/责任者:<\/dt><dd><a.*>(.*)[^<]\/(.*)<\/dd>.*出版发行项:<\/dt><dd>.*:(.*),(.*)<\/dd>.*<dt>ISBN/U'; */
     $preg = '/题名\/责任者:<\/dt><dd><a.*>(.*)[^<]\/(.*)<\/dd>.*出版发行项:<\/dt><dd>.*:(.*),(.*)<\/dd>.*<dt>ISBN/U';
-    /* echo $preg; */
     if(preg_match($preg, $contents, $out)){
-        /* print_r($out); */
         $title = preg_replace('/<\/a>?/','',$out[1]);   //某些情况不存在>符号，变量表示书名
         $auther = preg_replace('/ =.*/','',$out[2]);   //变量表示著作
         $press = $out[3];              //变量表示出版社
         $time = $out[4];           //变量表示出版时间
-        echo "书名：$title<br />作者：$auther<br />出版社：$press<br />出版时间：$time<br /><br />索书号：$search<br />馆藏地：$place<br />状态：$state<br />";
+        /* echo "书名：$title<br />作者：$auther<br />出版社：$press<br />出版时间：$time<br /><br />索书号：$search<br />馆藏地：$place<br />状态：$state<br />"; */
+        return array("res"=>200, "title"=>$title, "auther"=>$auther, "press"=>$press, "time"=>$time, "search"=>$search, "place"=>$place, "state"=>$state);
+    }else{
+        return array("res"=>201,"mes"=>"top");   //表示没有这本，并且是因为上面匹配为空导致，返回就直接终止
+        exit;
     }
 }
-
 
 /**
  * GET 请求
@@ -85,6 +129,7 @@ function http_get($url){
     $oCurl = curl_init();
     curl_setopt($oCurl, CURLOPT_URL, $url);
     curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($oCurl, CURLOPT_TIMEOUT,20);   //只需要设置一个秒的数量就可以  
     $sContent = curl_exec($oCurl);
     $aStatus = curl_getinfo($oCurl);
     curl_close($oCurl);
