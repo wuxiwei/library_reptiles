9575b4def9340ce0e29c806d063c67028c2bbba0
wxw <wxw_it@163.com>
2016-05-07 18:30:19 +0800

    图书表结构

--- library_books.sql
+++ library_books.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2016-05-06 02:22:03
+-- Generation Time: 2016-05-07 10:26:20
 -- 服务器版本： 5.5.47-MariaDB
 -- PHP Version: 5.6.20
 
@@ -27,15 +27,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `library_books` (
-  `id` int(11) NOT NULL COMMENT '自增量',
-  `no` int(11) NOT NULL COMMENT 'url编号',
-  `title` varchar(80) NOT NULL COMMENT '书名',
-  `auther` varchar(80) NOT NULL COMMENT '作者',
-  `press` varchar(80) NOT NULL COMMENT '出版社',
-  `time` varchar(20) NOT NULL COMMENT '出版日期',
-  `search` varchar(20) NOT NULL COMMENT '索书号',
-  `place` varchar(120) NOT NULL COMMENT '馆藏地',
-  `state` varchar(8) NOT NULL COMMENT '状态'
+          `id` int(11) NOT NULL COMMENT '自增量',
+          `no` int(11) NOT NULL COMMENT 'url编号',
+          `title` varchar(80) NOT NULL COMMENT '书名',
+          `auther` varchar(80) NOT NULL COMMENT '作者',
+          `press` varchar(80) NOT NULL COMMENT '出版社',
+          `time` varchar(8) NOT NULL COMMENT '出版日期',
+          `search` varchar(20) NOT NULL COMMENT '索书号',
+          `place` varchar(120) NOT NULL COMMENT '馆藏地',
+          `state` varchar(8) NOT NULL COMMENT '状态'
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
